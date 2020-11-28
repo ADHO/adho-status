3735c834181fb6ac91a3b223b65af5ae80b590f1
Simon Wiles <simonjwiles@gmail.com>
2020-11-27 22:44:58 -0800

    Add cli arg to get config from GitHub -- otherwise, use local

--- bin/run_checks_on_console.py
+++ bin/run_checks_on_console.py
@@ -4,18 +4,24 @@
     If all checks complete successfully, the return code will be 0 (success) -- otherwise, it will
     be 1 (error).  This makes it suitable for use from a cron job. """
 
+import argparse
+
 import regex
 import requests
 import yaml
 from wasabi import msg
 
 
-def get_checks():
+def get_config_from_github():
     config_url = "https://raw.githubusercontent.com/ADHO/adho-status/main/src/config.js"
-    services_object_regex = regex.compile(r"services\s*=\s*(\{(?:[^{}]|(?1))*\})")
-
     response = requests.get(config_url)
-    checks = services_object_regex.search(response.text)
+
+    return response.text
+
+
+def parse_config(config):
+    services_object_regex = regex.compile(r"services\s*=\s*(\{(?:[^{}]|(?1))*\})")
+    checks = services_object_regex.search(config)
 
     return yaml.safe_load(checks.group(1))
 
@@ -34,7 +40,25 @@ def do_check(check):
 
 def main():
     """ Command-line entry-point. """
-    checks = get_checks()
+    parser = argparse.ArgumentParser(description="Description: {}".format(__file__))
+
+    parser.add_argument(
+        "-g",
+        "--get-config-from-github",
+        action="store_true",
+        default=False,
+        help="Get the config.js file from the GitHub repository",
+    )
+
+    args = parser.parse_args()
+
+    if args.get_config_from_github:
+        config = get_config_from_github()
+    else:
+        with open("../src/config.js", "r") as _fh:
+            config = _fh.read()
+
+    checks = parse_config(config)
 
     if all(do_check(check) for check in checks.values()):
         raise SystemExit(0)
