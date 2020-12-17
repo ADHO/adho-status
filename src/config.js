5bb56f8f20b3337bd4d2d6302e4ae536e9fdfa2a
Simon Wiles <simonjwiles@gmail.com>
2020-12-16 21:47:37 -0800

    Update config to be grouped by sections

--- src/config.js
+++ src/config.js
@@ -1,30 +1,45 @@
 export const services = {
-  mainAdhoSite: {
-    displayName: "Main ADHO Site",
-    endpointUrl: "https://adho.org/",
-    needsCors: true,
-    serviceUrl: "https://adho.org/",
-    test: "testResponseForText",
-    matchText: "<title>Our Mission | ADHO</title>",
+  adhoDotOrg: {
+    heading: "adho.org",
+    services: {
+      mainAdhoSite: {
+        displayName: "Main ADHO Site",
+        endpointUrl: "https://adho.org/",
+        needsCors: true,
+        serviceUrl: "https://adho.org/",
+        test: "testResponseForText",
+        matchText: "<title>Our Mission | ADHO</title>",
+      },
+    },
   },
-  achWebsite: {
-    displayName: "ACH Website",
-    endpointUrl: "https://ach.org/",
-    needsCors: true,
-    serviceUrl: "https://ach.org/",
-    serviceIcon: "ach.svg",
-    test: "testResponseForText",
-    matchText:
-      "<title>The Association for Computers and the Humanities | </title>",
+  digitalHumanitiesDotOrg: {
+    heading: "digitalhumanities.org",
+    services: {
+      dhq: {
+        displayName: "DHQ Website",
+        endpointUrl: "https://digitalhumanities.org/dhq/",
+        needsCors: true,
+        serviceUrl: "https://digitalhumanities.org/dhq/",
+        serviceIcon: "",
+        test: "testResponseForText",
+        matchText: '<?xml version="1.0" encoding="UTF-8"?>',
+      },
+    },
   },
-  dhq: {
-    displayName: "DHQ Website",
-    endpointUrl: "https://digitalhumanities.org/dhq/",
-    needsCors: true,
-    serviceUrl: "https://digitalhumanities.org/dhq/",
-    serviceIcon: "",
-    test: "testResponseForText",
-    matchText: '<?xml version="1.0" encoding="UTF-8"?>',
+  others: {
+    heading: "Others",
+    services: {
+      achWebsite: {
+        displayName: "ACH Website",
+        endpointUrl: "https://ach.org/",
+        needsCors: true,
+        serviceUrl: "https://ach.org/",
+        serviceIcon: "ach.svg",
+        test: "testResponseForText",
+        matchText:
+          "<title>The Association for Computers and the Humanities | </title>",
+      },
+    },
   },
 };
 
