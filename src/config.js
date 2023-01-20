ed3a2b3730fc5dc59ba08e1fa759c2283c0f322b
Simon Wiles <simonjwiles@gmail.com>
2023-01-20 11:13:31 -0800

    Update status icons

--- src/config.js
+++ src/config.js
@@ -166,23 +166,52 @@ export const statuses = {
     message: "Checking status...",
   },
   up: {
-    icon: "✔",
-    message: "No known issues",
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M9 12l2 2l4 -4"></path>
+      <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z"></path>
+    </svg>`,
+    message: "No detected issues",
   },
   maintenance: {
-    icon: "⚒",
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <rect x="4" y="7" width="16" height="9" rx="1"></rect>
+      <path d="M7 16v4"></path>
+      <path d="M7.5 16l9 -9"></path>
+      <path d="M13.5 16l6.5 -6.5"></path>
+      <path d="M4 13.5l6.5 -6.5"></path>
+      <path d="M17 16v4"></path>
+      <path d="M5 20h4"></path>
+      <path d="M15 20h4"></path>
+      <path d="M17 7v-2"></path>
+      <path d="M7 7v-2"></path>
+    </svg>`,
     message: "Temporarily unavailable for maintenance",
   },
   issue: {
-    icon: "⚠️",
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12 9v2m0 4v.01"></path>
+      <path d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75"></path>
+    </svg>`,
     message: "There may be an issue with this site",
   },
   fail: {
-    icon: "❗",
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <circle cx="12" cy="12" r="9"></circle>
+      <path d="M12 9v4"></path>
+      <path d="M12 16v.01"></path>
+    </svg>`,
     message: "Site unavailable",
   },
   outage: {
-    icon: "🚫",
+    icon: `
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="9"></circle>
+      <line x1="5.7" y1="5.7" x2="18.3" y2="18.3"></line>
+    </svg>`,
     message: "Service is down",
   },
 };
