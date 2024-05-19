6ebbcca75069c73941229ca6ddeacf7c352a23f8
Simon Wiles <simonjwiles@gmail.com>
2024-05-18 21:33:05 -0700

    Add DHd2025 conference site

--- src/config.js
+++ src/config.js
@@ -199,6 +199,14 @@ export const services = {
   dhdConferences: {
     heading: "DHd Conference Sites",
     services: {
+      dhd2025: {
+        displayName: "DHd 2025 Conference Site",
+        endpointUrl: "https://dhd2025.dig-hum.de/wp-login.php",
+        serviceUrl: "https://dhd2025.dig-hum.de/wp-login.php",
+        test: "testResponseForText",
+        matchText:
+          '<form name="loginform" id="loginform" action="https://dhd2025.dig-hum.de/wp-login.php" method="post">',
+      },
       dhd2024: {
         displayName: "DHd 2024 Conference Site",
         endpointUrl: "https://dhd2024.dig-hum.de/wp-login.php",
